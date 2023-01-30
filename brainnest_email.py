5af3dc5985ee40546af44b46e8a6dd3a0dbc99af
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2023-01-30 14:37:34 +0100

    Added sender email iteration.

--- brainnest_email.py
+++ brainnest_email.py
@@ -7,7 +7,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-
+# sample sender email
+sender = "sender@gmail.com"
 
 # list of recipients
 receivers = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com'] # exemple
@@ -24,7 +25,7 @@ report_dir = '/Users/user/Desktop/projet/email' # exemple
 # iterate through recipients
 for recipient in receivers:
    msg = MIMEMultipart()
-   msg['From'] = username
+   msg['From'] = sender
    msg['To'] = recipient
    msg['Subject'] = subject
    msg.attach(MIMEText(body))
@@ -42,8 +43,6 @@ for report_file in os.listdir(report_dir):
    msg.attach(attachment)
 
 # sends the email to an internet machine. In this case, it sends it to the localhost.
-sender = "sender@gmail.com"
-
 try:
    smtpObj = smtplib.SMTP('localhost')
    smtpObj.sendmail(sender, receivers, message)        
