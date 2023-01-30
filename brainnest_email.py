b21e9f6aef1e95cfed11b91c2bd44cb76f6c8a4b
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2023-01-30 14:41:29 +0100

    Revert "Added sender email iteration."
    This reverts commit 73857bcff7b0d4a8ae4c77e12dc72e95119d52cc.

--- brainnest_email.py
+++ brainnest_email.py
@@ -7,8 +7,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-# sample sender email
-sender = "sender@gmail.com"
+
 
 # list of recipients
 receivers = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com'] # exemple
@@ -25,7 +24,7 @@ report_dir = '/Users/user/Desktop/projet/email' # exemple
 # iterate through recipients
 for recipient in receivers:
    msg = MIMEMultipart()
-   msg['From'] = sender
+   msg['From'] = username
    msg['To'] = recipient
    msg['Subject'] = subject
    msg.attach(MIMEText(body))
@@ -43,6 +42,8 @@ for report_file in os.listdir(report_dir):
    msg.attach(attachment)
 
 # sends the email to an internet machine. In this case, it sends it to the localhost.
+sender = "sender@gmail.com"
+
 try:
    smtpObj = smtplib.SMTP('localhost')
    smtpObj.sendmail(sender, receivers, message)        
